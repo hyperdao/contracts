27a2ac45ef7d7abe48a5e047b314ce64dbeaf275
YorkManny <62131400+YorkManny@users.noreply.github.com>
2020-04-27 23:05:47 +0800

    Update proxy.lua
    add new functions in proxy contract

--- proxy.lua
+++ proxy.lua
@@ -5,7 +5,7 @@ type State = 'NOT_INITED' | 'COMMON' | 'STOPPED'
 type Storage = {
 	admin:string,
 	state:string,
-	symbol:string,,
+	symbol:string,
     cdcContractAddr: string,	
 	feederContractAddr: string, ---
 	stableTokenAddr: string,
@@ -65,27 +65,40 @@ let function parse_at_least_args(arg: string, count: int, error_msg: string)
     return parsed
 end
 
-let function can_distribute(self:table,amount:string)
+let function can_distribute(self:table,wishedAmount:string,addedAmount:int)
     let sn_wishedAmount = safemath.safenumber(amount)
 	if not sn_wishedAmount then
 	   return error("whishAmoun is not a number")
 	end
 	let sn_0 = safemath.safenumber(0)
 	if safemath.number_lte(sn_wishedAmount,sn_0) then
-		return error("whishedAmount must > 0")
+		return error("wishedAmount must > 0")
 	end
 	
 	
-    let priceFeederContract:object = import_contract_from_address(self.storage.cdcContractAddr)
-	let liquidationRatio = liquidationRatio:liquidationRatio('')
+    let cdcContractAddr:object = import_contract_from_address(self.storage.cdcContractAddr) ---get the cdc object
+	let liquidationRatio = cdcContractAddr:liquidationRatio('')
 	let sn_liquidationRatio = safemath.safenumber(liquidationRatio)
 	
-    return false
+	let feederContractAddr:object = import_contract_from_address(self.storage.feederContractAddr)
+	let price = feederContractAddr:getPrice('')
+	let sn_price = safemath.safenumber(price)
+    
+    let sn_totalvalue_wished = safemath.safe_number_multiply(sn_price,sn_wishedAmount)
+	let sn_addedAmount = safemath.safenumber(addedAmount)
+	let sn_totalvalue_collectal = safemath.safe_number_multiply(sn_price,sn_addedAmount)
+	
+	let sn_ratio = safemath.safe_number_div(sn_totalvalue_collectal,sn_totalvalue_wished)
+	if safemath.safe_number_lt(sn_ratio,sn_liquidationRatio) then
+	    return error("u need amount exceed reality")
+	end    	
+    return true
 end
 
 
-let function mint(self:table,amount: int)
-
+let function mint(self:table,address:string,amount: string)
+    let stableTokenAddrContract:object = import_contract_from_address(self.storage.stableTokenAddrContract)
+	stableTokenAddrContract:mint(address..','..amount)
 end
 
 
@@ -147,14 +160,17 @@ function M:on_deposit_asset_imp(jsonstrArgs:string)
 	let parsed:Array<string> = totable(parse_args(param,2,"arg format error, need format:openCdc,amount or addCollateral,cdc_id"))
     if parsed[1] == 'openCdc' then
 	---warning to implement
+	    let wishedAmount = parsed[2]
+	    can_distribute(self,wishedAmount,addedAmount)
 	    cdc_info = {}
 		cdc_info['owner'] = from_address
 		cdc_info['collateralAmount'] = addedAmount
-		cdc_info['stableTokenAmount'] = tointeger(parsed[2])
+		cdc_info['stableTokenAmount'] = tointeger(wishedAmount)
 		cdc_info['secSinceEpoch'] = tointeger(get_chain_now() or 0)
 		cdc_id = tostring('12213434')
 		fast_map_set("cdc",cdc_id,json.dumps(cdc_info))
 		cdc_info['cdcId'] = cdc_id
+		mint(self,from_address,wishedAmount)
 		emit OpenCdc(json.dumps(cdc_info))
 	    return  cdc_id
     end
@@ -170,15 +186,42 @@ function M:on_deposit_asset_imp(jsonstrArgs:string)
 		end
 	    cdc_info['collateralAmount'] = tointeger(cdc_info['collateralAmount']) + addedAmount
 		fast_map_set("cdc",cdc_id,json.dumps(cdc_info))
-		emit AddCollateral(json.dumps({cdc_id:cdc_id,addAmount:addAmount}))
+		emit AddCollateral(json.dumps({cdc_id:cdc_id,addAmount:addedAmount}))
 	    return "OK"---need emit
 	end
 end
 
+function M:expandLoan(args:string)
 
+end
 
+function M:getStabilityFee(cdc_id:string)
+    let cdc_info_obj = fast_map_get("cdc",cdc_id)
+	if cdc_info_obj == nil then
+		return error("cdc not exist , cdc_id:"..cdc_id)
+	end
+	let cdc_info = totable(json.loads(tostring(cdc_info_obj)))
+	let stableTokenAmount = cdc_info['stableTokenAmount']
+	let secPoint = cdc_info['secSinceEpoch']
+	let sn_stableTokenAmount = safemath.safenumber(stableTokenAmount)
+	let sn_secPoint = safemath.safenumber(sn_stableTokenAmount)
+	let time_now = tointeger(get_chain_now() or 0)
+	let sn_time_now = safemath.safenumber(time_now)
+	
+	let sn_daysec = safemath.safenumber(86400)
+	let sn_year  = safemath.safenumber(365)
+	
+	let sn_length = safemath.safe_number_minus(sn_time_now,sn_secPoint)
+	let sn_day = safemath.safe_number_idiv(sn_length,sn_daysec)
+	
+	
+	let cdcContractAddr:object = import_contract_from_address(self.storage.cdcContractAddr)
+	let annualStabilityFee = cdcContractAddr:annualStabilityFee('')
+	let sn_annualStabilityFee = safemath.safenumber(annualStabilityFee)
+	let sn_stablityFee_t = safemath.safe_number_multiply(sn_day,sn_annualStabilityFee)
+	let sn_stablityFee = safemath.safe_number_idiv(sn_stablityFee_t,sn_year)
+	return sn_stablityFee
+end
 
 
 return M
-
-
